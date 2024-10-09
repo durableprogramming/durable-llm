b6d8a1ff2131233310fe850d995e39377ee202c0
Durable Programming Team <djberube@durableprogramming.com>
2024-10-09 19:01:45 -0400

    add lib/durable.rb

---
+++ lib/durable.rb
@@ -0,0 +1,7 @@
+require 'durable/llm'
+
+module Durable
+  # This module serves as a namespace for the Durable gem.
+  # It currently only requires the Llm module, but can be expanded
+  # in the future to include other Durable-related functionality.
+end
